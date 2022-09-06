ba5933ca53fba63271e97d928ea64aaae3bf6c87
milky <milky@192.168.0.201>
2022-09-06 18:51:16 +0100

    Er på siste delen av oppgave B5

--- Java Programmeringsopgave 1 - Obligatorisk Innlevering/src/oblig/OppgaveB5_Karkaterskala.java
+++ Java Programmeringsopgave 1 - Obligatorisk Innlevering/src/oblig/OppgaveB5_Karkaterskala.java
@@ -4,6 +4,7 @@ import static java.lang.Integer.*;
 import static javax.swing.JOptionPane.showInputDialog;
 import static javax.swing.JOptionPane.showMessageDialog;
 
+
 public class OppgaveB5_Karkaterskala {
 	public static void main(String[] args) {
 		
