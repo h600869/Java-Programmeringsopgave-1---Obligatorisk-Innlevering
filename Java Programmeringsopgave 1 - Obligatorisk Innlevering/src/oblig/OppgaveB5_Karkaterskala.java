3253a5dec85b34888acf5e33ca2e92e317eaeb9c
milky <milky@192.168.0.201>
2022-09-06 18:17:52 +0100

    gfsf

--- Java Programmeringsopgave 1 - Obligatorisk Innlevering/src/oblig/OppgaveB5_Karkaterskala.java
+++ Java Programmeringsopgave 1 - Obligatorisk Innlevering/src/oblig/OppgaveB5_Karkaterskala.java
@@ -1,5 +1,10 @@
 package oblig;
 
-public class OppgaveB5_Karkaterskala {
+import static java.lang.Integer.*;
+import static javax.swing.JOptionPane.showInputDialog;
+import static javax.swing.JOptionPane.showMessageDialog;
 
+public class OppgaveB5_Karkaterskala {
+	
+	String Poeng = showInputDialog(" Hvormange Poeng fikk du? : ");
 }
