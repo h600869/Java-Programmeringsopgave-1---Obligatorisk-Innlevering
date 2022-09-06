8e27a4635e1fbf618421e477afb5bb2b490126f2
milky <milky@192.168.0.201>
2022-09-06 18:45:09 +0100

    Ferdig med b-delen av oppgave B5

--- Java Programmeringsopgave 1 - Obligatorisk Innlevering/src/oblig/OppgaveB5_Karkaterskala.java
+++ Java Programmeringsopgave 1 - Obligatorisk Innlevering/src/oblig/OppgaveB5_Karkaterskala.java
@@ -6,14 +6,36 @@ import static javax.swing.JOptionPane.showMessageDialog;
 
 public class OppgaveB5_Karkaterskala {
 	public static void main(String[] args) {
+		
+		for ( int i=1;i <= 10; i++) {
+			
 		String Poeng = showInputDialog(" Hvor mange poeng fikk du? ");
 		
 		int resultat = Integer.parseInt(Poeng);
 		
-		if (resultat<0 && resultat > 100 ) {
-			showMessageDialog(null, "Ugyldig program. Prøv igjen");
+			
+			
+			if (resultat<0 && resultat > 100 ) {
+			showMessageDialog(null, "Ugyldig Poeng. Prøv igjen");
+			}
+			else if (resultat >=0 && resultat <= 39) {
+			showMessageDialog(null, "Karakter = F" );
+			}
+			else if (resultat >=40 && resultat <=49) {
+			showMessageDialog(null,"Karakter = E");
+			}
+			else if (resultat>=50 && resultat<=59) {
+			showMessageDialog(null, "Karakter = D");
+			}
+			else if (resultat>=60 && resultat <=79) {
+			showMessageDialog(null, "Karakter = C");
+			}
+			else if (resultat>=80 && resultat<= 89) {
+			showMessageDialog(null, "Karakter = B");
+			}
+			else if (resultat>=90 && resultat <=100) {
+			showMessageDialog(null, "Karkater = A");
+			}
 		}
-		
-		showMessageDialog(null, " Karkater : " + resultat);
 	}
 }
