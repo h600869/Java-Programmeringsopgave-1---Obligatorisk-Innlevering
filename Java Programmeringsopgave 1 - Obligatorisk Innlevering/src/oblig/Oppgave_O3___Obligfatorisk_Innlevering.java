593df733ee8915c7bc29f5aeac7b325a1b0dfe0e
milky <milky@192.168.0.201>
2022-09-07 12:21:16 +0100

    Ferdig med alle oppgavene. Klarte ikke å sette kontroll på andre oppgave

--- Java Programmeringsopgave 1 - Obligatorisk Innlevering/src/oblig/Oppgave_O3___Obligfatorisk_Innlevering.java
+++ Java Programmeringsopgave 1 - Obligatorisk Innlevering/src/oblig/Oppgave_O3___Obligfatorisk_Innlevering.java
@@ -1,10 +1,27 @@
 package oblig;
 
+import java.util.Scanner;
+
 public class Oppgave_O3___Obligfatorisk_Innlevering {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		int tall;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Skriv inn tallet: ");
+		
+		
+		tall = sc.nextInt();
+		sc.close();
+		
+		long fact = 1;
+		int i = 1;
+		
+		while (i<=tall) 
+		{
+			fact = fact * i;
+			i++;
+		}
+		System.out.println("Fakultet av " + tall + " er " + fact);
 	}
-
 }
